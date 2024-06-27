41feb763bdeecd2ef4cdf64ecf66cb2d38d4349d
Alexander Vielma <vielmaa1804@gmail.com>
2024-06-27 11:45:26 -0400

    Add my cv to cv-button

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const ABOUT_SECTION = document.querySelector("#about")
 const ARROW_UP = document.querySelector(".arrow-up");
+const CV_BTN = document.querySelector(".cv-button")
 
 
-const changeOpacity = (strNumber) => {
-    let number = Number(strNumber)
-    if (number > 260) {
+
+
+CV_BTN.addEventListener("click", (e) => {
+    downloadCv(e.target)
+})
+
+
+const changeArrowUpOpacity = (strNumber) => {
+    let yCordinates = Number(strNumber)
+    if (yCordinates > 260) {
         ARROW_UP.style.opacity = 0;
     } else {
-        ARROW_UP.style.opacity = 1;
+        ARROW_UP.style.opacity = 1;x
     }
 }
 
 window.addEventListener("scroll", () => {
     const cordinates = ABOUT_SECTION.getBoundingClientRect();
     let y = cordinates.y
-    changeOpacity(y.toFixed(0))
+    changeArrowUpOpacity(y.toFixed(0))
 })
